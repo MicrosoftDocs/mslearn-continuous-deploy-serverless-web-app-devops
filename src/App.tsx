4dd7a47d551d7c5642dce4636ab41ba14a0e6111
Alistair Matthews <alistairm@contentmaster.com>
2020-01-07 17:29:25 +0000

    Initial code upload

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>AdventureWorks Insurance</h1>
+      <p>Get your quote online</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
